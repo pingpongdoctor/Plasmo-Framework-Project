19b3d84c8eeca09593f0bd50ab19abc0653930f8
Simon Tran <thanhnhantran1501@gmail.com>
2023-11-09 16:44:34 -0800

    feat: successfully connect mongodb database by using mongoose

---
+++ lib/databaseConnect.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/test";
+
+if (!URI) throw new Error("Please add your MongoURI to .env");
+
+//CONNECT DB FUNCTION
+export const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(URI);
+    console.log("CONNECTED TO MONGODB");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//DISCONNECT DB FUNCTION
+export const disconnectMongoDB = async () => {
+  try {
+    await mongoose.disconnect();
+    console.log("DISCONNECTED FROM MONGODB");
+  } catch (error) {
+    console.log(error);
+  }
+};
