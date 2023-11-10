ce91b62522183751f413280bd01b2db167f33953
Simon Tran <thanhnhantran1501@gmail.com>
2023-11-10 14:48:17 -0800

    feat: add css variables for fonts in tailwind config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/_styles/globals.css";
+import { inter, roboto_mono } from "../_fonts/fonts";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${inter.variable} ${roboto_mono.variable}`}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
