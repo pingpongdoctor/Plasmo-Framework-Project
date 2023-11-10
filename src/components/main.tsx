6495e4bca1f5f80475535e32894bcd53fb04fc35
Simon Tran <thanhnhantran1501@gmail.com>
2023-11-09 16:04:15 -0800

    feat: set up plasmo framework and popup component

---
+++ src/components/main.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+interface Props {
+  name?: string;
+}
+
+export function Main({ name = "Extension" }: Props) {
+  const [data, setData] = useState("");
+
+  return (
+    <div className="w-[300px] h-[300px]">
+      <h1>
+        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> {name}!
+      </h1>
+      <input onChange={(e) => setData(e.target.value)} value={data} />
+
+      <a href="https://docs.plasmo.com">READ THE DOCS!</a>
+    </div>
+  );
+}
