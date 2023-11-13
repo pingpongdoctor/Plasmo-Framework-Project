db635fc9a2de7508270407650c0f70d14cc6329b
Simon Tran <thanhnhantran1501@gmail.com>
2023-11-13 15:39:39 -0800

    feat: set up auth0

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/_styles/globals.css";
 import { inter, roboto_mono } from "../_fonts/fonts";
 import type { AppProps } from "next/app";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${inter.className} ${roboto_mono.variable}`}>
-      <Component {...pageProps} />;
-    </main>
+    <UserProvider>
+      <main className={`${inter.className} ${roboto_mono.variable}`}>
+        <Component {...pageProps} />;
+      </main>
+    </UserProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,44 @@
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Link from "next/link";
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+import { Translation } from "../../mongo/interface";
+import { getSession } from "@auth0/nextjs-auth0";
+
+interface UserData {
+  _id: string;
+  name: string;
+  translations: Translation & { _id: string };
+}
+
+export const getServerSideProps = async (context: any) => {
+  const session = await getSession(context.req, context.res);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/api/logout",
+        permanent: false,
+      },
+    };
+  }
+};
+// satisfies GetServerSideProps<{
+//   userData: UserData;
+// }>;
+
 export default function Home() {
-  return (
-    <div>
-      <h1 className="plasmo-text-red-400 plasmo-font-roboto">
-        Translate Text Chrome Extension
-      </h1>
-    </div>
-  );
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  if (user) {
+    return (
+      <div>
+        <h1 className="plasmo-text-red-400 plasmo-font-roboto">
+          Translate Text Chrome Extension
+        </h1>
+      </div>
+    );
+  }
 }
