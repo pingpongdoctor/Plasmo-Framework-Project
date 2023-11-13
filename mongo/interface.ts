df2427ee4d1c7eb3a089f6951fc9dea93d525a4b
Simon Tran <thanhnhantran1501@gmail.com>
2023-11-13 14:19:17 -0800

    feat: create one to many realtionship between user and translation schemas

--- mongo/interface.ts
+++ mongo/interface.ts
@@ -1,6 +1,15 @@
-export interface Translations {
+import { Types } from "mongoose";
+
+export interface Translation {
   from: string;
   to: string;
   originalContent: string;
   translatedContent: string;
+  user: Types.ObjectId;
+}
+
+export interface User {
+  name: string;
+  googleId: string;
+  translations: Types.ObjectId[];
 }
