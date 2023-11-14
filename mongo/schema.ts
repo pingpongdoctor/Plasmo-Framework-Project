8b9176b85f62fedca32c0418ce3b832d56ed40d7
Simon Tran <thanhnhantran1501@gmail.com>
2023-11-13 18:02:46 -0800

    fix: fix bugs in persist data endpoint

--- mongo/schema.ts
+++ mongo/schema.ts
@@ -4,7 +4,7 @@ import { Translation, User } from "./interface";
 const userSchema = new Schema<User>(
   {
     name: { type: String, required: true },
-    authId: { type: String, required: true },
+    auth0Id: { type: String, required: true },
     email: { type: String, required: true },
     translations: [
       { type: Schema.Types.ObjectId, ref: "Translation", default: [] },
